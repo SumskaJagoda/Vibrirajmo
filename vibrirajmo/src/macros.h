de53f142b5cdd663b4fdc57dc0e1918e6e1f2bd3
epicfooxlinux <lukafucek11@gmail.com>
2014-03-24 14:08:16 +0100

    first commit

---
+++ vibrirajmo/src/macros.h
@@ -0,0 +1,11 @@
+// board addresses
+#define DEVICE1_ADDR 0x40
+#define DEVICE2_ADDR 0x41
+
+// Registers
+#define MOD1_REG 0x00 // MOD1 register
+
+#define ALL_ON_L 0xfa
+#define ALL_ON_H 0xfb
+#define ALL_OFF_L 0xfc
+#define ALL_OFF_H 0xfd
---
+++ vibrirajmo/src/main.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : sumska jagoda
+// Version     : 0
+// Copyright   : nase i samo nase
+// Description : main
+//============================================================================
+//#include <dos.h>
+#include "I2CDevice.h"
+#include <iostream>
+#include "macros.h"
+using namespace std;
+
+
+//#define ALL_ADR 0xfa
+
+int main() {
+
+
+I2CDevice *i2cptr = new I2CDevice(1,DEVICE1_ADDR);
+	//init
+	i2cptr->writeByte(MOD1_REG,0x01); //sleep off
+	i2cptr->writeByte(0xfd,0x00);
+
+
+
+	i2cptr->writeByte(0xfa,0x01);
+	cout << "RADII! :D:D" << endl; // znak da radi :D
+	delete i2cptr;
+	return 0;
+}
